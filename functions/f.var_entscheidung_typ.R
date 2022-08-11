a41d411fef9ed05af20e445e292bcc9af5c711bb
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-08-11 04:00:53 +0200

    add var entscheidung fun

---
+++ functions/f.var_entscheidung_typ.R
@@ -0,0 +1,47 @@
+#' Entscheidungstyp extrahieren
+#'
+#' Durchsucht den Volltext einer Gerichtsentscheidung auf das Vorhandensein der Wörter "Beschluss" oder "Urteil". Der erste Treffer stellt den Typ der Entscheidung dar.
+
+
+#' @param x Character. Ein Vektor, die den Volltext von Gerichtsentscheidungen enthalten.
+#'
+#' @param return Character. Ein Vektor aller Entscheidungstypen. Entweder "B" für Beschluss oder "U" für Urteil.
+
+
+f.var_entscheidung_typ <- function(x){
+
+    ## Entscheidungstyp extrahieren
+    entscheidung_typ <- stringi::stri_extract_first(str = x,
+                                                    regex = paste0(gsub(" ", " *", "B E S C H L U S S"),
+                                                                   "|",
+                                                                   gsub(" ", " *", "B E S C H L U ß"),
+                                                                   "|",
+                                                                   gsub(" ", " *", "U R T E I L"),
+                                                                   "|",
+                                                                   gsub(" ", " *", "V E R F Ü G U N G")),
+                                                    case_insensitive = TRUE)
+
+    ## Auf einzelnen Buchstaben reduzieren
+    entscheidung_typ <- gsub("([BUV]).+",
+                             "\\1",
+                             toupper(entscheidung_typ))
+
+    ## Unit Test: Vollständigkeit der Extraktion
+    if (!length(x) == length(entscheidung_typ)){
+
+        stop("Extraktion nicht vollständig.")
+        
+    }
+    
+    ## Unit Test: Begrenzung auf bestimmte Entscheidungstypen
+    if (any(!entscheidung_typ %in% c("B", "U", "V"))){
+
+        stop("Entscheidungstypen entsprechen nicht der Definition.")
+        
+    }
+
+    
+    return(entscheidung_typ)
+    
+    
+}
