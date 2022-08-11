dbfa8e86edccdbbe1f273e8cb79201830a224593
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-08-11 21:03:06 +0200

    rewrite ecli fun

--- functions/f.var_ecli_bgh.R
+++ functions/f.var_ecli_bgh.R
@@ -1,9 +1,9 @@
 #' Experimentelle Erstellung von ECLIs für Entscheidungen des Bundesgerichtshofs.
 
 
-#' Struktur und Inhalt der ECLI für deutsche Gerichte sind auf dem Europäischen Justizportal näher erläutert. \footnote{\url{https://e-justice.europa.eu/content_european_case_law_identifier_ecli-175-de-de.do?member=1}}
+#' Struktur und Inhalt der ECLI für deutsche Gerichte sind auf dem Europäischen Justizportal näher erläutert: https://e-justice.europa.eu/content_european_case_law_identifier_ecli-175-de-de.do?member=1
 #'
-#' Achtung: alle ECLIs die sich nur in der Kollisionsziffer unterscheiden sind potentiell fehlerhaft, da mir aktuell keine Möglichkeit bekannt ist, die originale Vergabe der Kollisionsziffer zu reproduzieren.
+#' ACHTUNG: diese ECLIs sind experimentell. Der BGH vergibt offiziell ECLI-Identifikatoren nur für Entscheidungen ab 2016. Ausserdem steht die originale Kollisions-ID nicht zur Verfügung. Alle Entscheidungen mit einer Kollisions-ID größer 0 und ihre korrespondierenden Entscheidungen mit der ID 0 haben potenziell syntaktisch korrekte, aber semantisch fehlerhafte ECLIs.
 
 
 #' @param x Data.table. Ein BGH-Datensatz mit den Variablen "datum", "entscheidung_typ", "spruchkoerper_az", "registerzeichen", "eingangsnummer", "eingangsjahr_az", "verzoegerung" und "kollision".
@@ -15,28 +15,51 @@
 
 
 
+
+
 f.var_ecli_BGH <- function(x,
-                              dt.download){
+                           az.brd){
 
 
-    ## Ordinalzahl erstellen
+
+
+    ## Originale Registerzeichen wiederherstellen
+
+    ecli.registerzeichen <- mgsub::mgsub(x$registerzeichen,
+                                         az.brd$zeichen_code,
+                                         az.brd$zeichen_original)
+    
+    ## Formatieren der Registerzeichen für ECLI
+    
+    ecli.registerzeichen <- gsub("\\(|\\)",
+                                 "\\.",
+                                 ecli.registerzeichen)
+    
+    ecli.registerzeichen <- toupper(ecli.registerzeichen)
+    
+    
+
+
+
+    ## Erstellen der ECLI-Ordinalzahl
 
     ecli.ordinalzahl <- paste0(format(x$datum,
                                       "%d%m%y"),
                                x$entscheidung_typ,
-                               x$spruchkoerper_az,
-                               gsub("-",
-                                    "",
-                                    x$registerzeichen),
+                               ifelse(is.na(x$spruchkoerper_az),
+                                      "",
+                                      x$spruchkoerper_az),
+                               ecli.registerzeichen,
                                x$eingangsnummer,
                                ".",
                                formatC(x$eingangsjahr_az,
                                        width = 2,
                                        flag = "0"),
-                               ifelse(is.na(x$verzoegerung),
-                                      ".",
-                                      x$verzoegerung),
+                               ".",
                                x$kollision)
+
+
+
 
 
     ecli.ordinalzahl <- gsub("NA",
@@ -44,100 +67,45 @@ f.var_ecli_BGH <- function(x,
                              ecli.ordinalzahl)
 
 
-
-
-    
-
-
-    ## === ECLI testen ===
-
-
-    ## --- Tabelle der Soll- und Ist-Ordinalzahlen vorbereiten ---
-    ## Die im vorigen Schritt generierten ECLI-Ordinalzahlen ("ist") werden nun noch einmal mit den in den Links angegebenen ECLI-Ordinalzahlen ("soll") gegenübergestellt um sicherzustellen, dass es sich um gültige ECLIs handelt.
-    
-    ## Berücksichtigt werden im folgenden nur Dateien die tatsächlich heruntergeladen wurden.
-    
-    files.pdf <- list.files("pdf",
-                            pattern = "\\.pdf$",
-                            ignore.case = TRUE)
-
-    eclitest.index <- dt.download$doc_id %in% files.pdf
-
-    eclitest.links <- dt.download$url[eclitest.index]
-    eclitest.pdf <- dt.download$doc_id[eclitest.index]
-
-    soll <- basename(eclitest.links)
-
-
-    ## Bei einigen PDF-Dateien sind die Soll-Ordinalzahlen (z.B. "010806B1WDS-VR3.06.0" und "130607B1WDS-VR2.07.0") in den Links fehlerhaft, weil Bindestriche im ECLI-System nicht vorgesehen sind. Für diese Dateien werden vor dem Abgleich Korrekturen vorgenommen.
-
-    soll <- gsub("\\.pdf",
-                 "",
-                 soll)
-
-    soll <- gsub(";",
-                 "",
-                 soll)
-
-    soll <- gsub("WDS-([VRAVPKH]{1,3})",
-                 "WDS\\1",
-                 soll)
-
-    soll <- gsub("D-PKH",
-                 "DPKH",
-                 soll)
-
-    
-
-    ## Bei diesen Entscheidungen sind die Links fehlerhaft.  Die korrekten ECLI-Ordinalzahlen für die beiden Entscheidungen stammen jeweils aus dem Text des PDF-Dokumentes.
-    
-    soll <- gsub("030206BPKH1.062.06.0",
-                 "030206B8PKH1.06.0",
-                 soll)
-    
-    soll <- gsub("190405BVR1011.041012.04.0",
-                 "190405B4VR1011.04.0",
-                 soll)
-    
-
-    ## Tabelle der Soll- und Ist-Ordinalzahlen erstellen
-
-    ist <- ecli.ordinalzahl
-
-    soll <- soll[match(files.pdf,
-                       eclitest.pdf)]
-
-
-    ecli.o.check <- data.table(soll,
-                               ist)
-
-
-
-    ## ECLI-Test durchführen
-
-    test.result <- identical(ecli.o.check$soll,
-                             ecli.o.check$ist)
-    
-
-    if (test.result == FALSE){
-
-        diff <- setdiff(ecli.o.check$soll, ecli.o.check$ist)
-        fehlerhaft <- ecli.o.check[soll %in% diff]
-        
-        warning("Folgende ECLIs sind fehlerhaft:")
-        warning(fehlerhaft)
-        
-        stop("REGEX-Test gescheitert. ECLIs sind fehlerhaft.")
-    }
-
-
-    
     ## Vollständige ECLI erstellen
     
     ecli <- paste0("ECLI:DE:BGH:",
                    x$entscheidungsjahr,
                    ":",
                    ecli.ordinalzahl)
+
+
+    
+    ## REGEX-Validierung: Gesamte ECLI
+
+    regex.test <- grep(paste0("ECLI:DE:BGH", # Präamel
+                              ":[0-9]{4}:", # Entscheidungsjahr
+                              "[0-9]{6}", # Datum
+                              "[IVX0-9a-z]*", # Senatsnummer
+                              "[A-Z.]+", # Registerzeichen
+                              "[0-9]+", # Eingangsnummer
+                              "\\.[0-9]{2}", # Eingangsjahr
+                              "\\.[0-9]"), # Kollisionsziffer
+                              
+                       ecli,
+                       value = TRUE,
+                       invert = TRUE)
+
+
+    ## Fehlerhafte ECLI
+
+    if(length(regex.test) != 0){
+        warning("Fehlerhafte ECLI:")
+        warning(regex.test)
+    }
+
+    ## Unit Test
+    test_that("ECLI entsprechen Erwartungen.", {
+        expect_type(ecli, "character")
+        expect_length(regex.test,  0)
+        expect_length(ecli, nrow(x))
+    })
+    
 
     
     return(ecli)
