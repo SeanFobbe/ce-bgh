b571017e237536cd211d670e6b0604e4cbdf9b63
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-04 21:32:09 +0100

    remove pull flag from docker

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-time docker build --pull -t ce-bgh:4.2.2 .
+time docker build -t ce-bgh:4.2.2 .
--- docker-run-project.sh
+++ docker-run-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-time docker build --pull -t ce-bgh:4.2.2 .
+time docker build -t ce-bgh:4.2.2 .
 
 time docker-compose run --rm ce-bgh Rscript run_project.R
