a8591949b1ef78d93fb0547a7943ec6e24a99ba4
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-04 20:55:27 +0100

    update download fun

--- functions/f.download.R
+++ functions/f.download.R
@@ -18,9 +18,11 @@
 #' @return Character. A vector of the downloaded file names.
 
 
+
 f.download <- function(url,
                        filename,
                        dir,
+                       clean = TRUE,
                        sleep.min = 0,
                        sleep.max = 0.1,
                        retries = 3,
@@ -29,92 +31,103 @@ f.download <- function(url,
                        timeout = 300,
                        debug.toggle = FALSE,
                        debug.files = 500){
+
+
+    
+    ## Set timeout for downloads
+    options(timeout = timeout)
+
+    
     
     ## Test for equality of length
     if(length(url) != length(filename)){
 
-        stop("Length of arguments url and filename is not equal.")
+        stop("Length of arguments 'url' and 'filename' is not equal.")
         
     }
 
-    ## Set timeout for downloads
-    options(timeout = timeout)
+    
+    ## Create download folder
+    dir.create(dir, showWarnings = FALSE, recursive = TRUE)
+
+
+    
+    ## Create data frame
+    df <- data.frame(url, filename)
+
 
     ## [Debugging Mode] Reduce download scope
     if (debug.toggle == TRUE){
 
-        sample.index <- sample(length(url), debug.files)
-
-        url <- url[sample.index]
-        filename <- filename[sample.index]
+        sample.index <- sort(sample(nrow(df), debug.files))
+        df <- df[sample.index,]
 
     }
 
-    ## Create folder
-    dir.create(dir, showWarnings = FALSE)
-
+    
     ## Clean folder: Only files included in 'filename' may be present
-    files.all <- list.files(dir, full.names = TRUE)
-    delete <- setdiff(files.all, file.path(dir, filename))
-    unlink(delete)
 
+    if (clean == TRUE){
+        
+        files.all <- list.files(dir, full.names = TRUE)
+        delete <- setdiff(files.all, file.path(dir, df$filename))
+        unlink(delete)
+
+    }
+
+
+    
     ## Exclude already downloaded files
+    
     files.present <- list.files(dir)
-    filename.todo <- setdiff(filename, files.present)
-    url.todo  <- url[filename %in% filename.todo]
+    filename.todo <- setdiff(df$filename, files.present)
+    df.todo  <- df[df$filename %in% filename.todo,]
+    df.todo <- df.todo[!is.na(df.todo$url),] # skip NA urls
+
     
     
     ## Download: First Try
-    for (i in sample(length(url.todo))){
-        tryCatch({download.file(url = url.todo[i],
-                                destfile = file.path(dir,
-                                                     filename.todo[i]))
-        },
-        error = function(cond) {
-            return(NA)}
-        )
-        
-        Sys.sleep(runif(1, sleep.min, sleep.max))
-    }
 
+
+    if(nrow(df.todo) > 0){
+        
+        result.todo <- mapply(f.download_robust,
+                              url = df.todo$url,
+                              destfile = file.path(dir, df.todo$filename),
+                              sleep.min = sleep.min,
+                              sleep.max = sleep.max)
+
+    }
     
 
 
     ## Download: Retries
 
     for(i in 1:retries){
-
-        ## Missing files
-        files.present <- list.files(dir)
-        filename.missing <- setdiff(filename, files.present)
-        url.missing  <- url[filename %in% filename.missing]
-
-        if(length(filename.missing) > 0){
-            
-            for (i in 1:length(filename.missing)){
-                
-                response <- GET(url.missing[i])
-                
-                Sys.sleep(runif(1, 0.25, 0.75))
-                
-                if (response$status_code == 200){
-                    tryCatch({download.file(url = url.missing[i],
-                                            destfile = file.path(dir,
-                                                                 filename.missing[i]))
-                    },
-                    error = function(cond) {
-                        return(NA)}
-                    )     
-                }else{
-                    message("Response code is not 200.")  
-                }
-                
-                Sys.sleep(runif(1, retry.sleep.min, retry.sleep.max))
-            } 
-        }
         
-    }
+        files.present <- list.files(dir)
+        filename.missing <- setdiff(df$filename, files.present)
+        df.missing  <- df[df$filename %in% filename.missing,]
+        df.missing <- df.missing[!is.na(df.missing$url),] # skip NA urls
 
+
+        if(nrow(df.missing) > 0){
+            
+            result.retry <- mapply(f.download_robust,
+                                   url = df.missing$url,
+                                   destfile = file.path(dir, df.missing$filename),
+                                   sleep.min = retry.sleep.min,
+                                   sleep.max = retry.sleep.max)
+
+        }
+
+    }
+    
+
+
+
+    ## Report Missing Files
+    
     if(length(filename.missing) > 0){
 
         warning(paste("Missing file:", filename.missing, collapse = "\n"))
@@ -122,11 +135,50 @@ f.download <- function(url,
     }
 
 
+    
     ## Final File Count
+    
     files.all <- list.files(dir, full.names = TRUE)
-
     message(paste("Downloaded", length(files.all), "of", length(filename), "files."))
+
 
     return(files.all)
     
+}
+
+
+
+
+
+
+
+#' Wrapper function for download.file that returns 'NA' instead of an error when a file is not available.
+#'
+#' @param url A valid URL.
+#' @param destfile The destination file.
+#' @param sleep.min Positive Integer. Minimum number of seconds to randomly sleep between requests.
+#' @param sleep.max Positive Integer. Maximum number of seconds to randomly sleep between requests.
+#'
+#' @return Character. Returns destination file invisibly.
+
+
+
+
+f.download_robust <- function(url,
+                              destfile,
+                              sleep.min = 0,
+                              sleep.max = 0){
+
+    tryCatch({download.file(url = url,
+                            destfile = destfile)
+    },
+    error = function(cond) {
+        return(NA)}
+
+    )
+
+    Sys.sleep(runif(1, sleep.min, sleep.max))
+
+    invisible(destfile)
+
 }
