e3c96501ee6724aed0ad2929556c59d350e64132
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-21 01:38:07 +0200

    create new unified f.tar_lingstats function

---
+++ functions/f.tar_lingstats.R
@@ -0,0 +1,190 @@
+#' Parallel computation of linguistic statistics
+
+#' Iterated parallel computation of characters, tokens, types and sentences for each document of a given data table. Compatible with targets framework.
+
+#' @param x Data.table. Must contain texts in a "text" variable and document names in a "doc_id" variable.
+#' @param multicore Logical. Whether to process each document sequentially or to use multiple cores. Defaults to TRUE.
+#' @param cores Positive integer. Number of cores to be used. Defaults to 2.
+#' @param germanvars Logical. Whether to return variable names in German. Defaults to FALSE.
+
+#' library(quanteda)
+#' library(future)
+#' library(future.apply)
+#' library(data.table)
+
+
+f.lingstats <- function(x,
+                        multicore = TRUE,
+                        cores = 2,
+                        germanvars = FALSE,
+                        tokens_locale = "en"){
+
+    ## Set Future Strategy
+    if(multicore == TRUE){
+
+        plan("multicore",
+             workers = cores)
+        
+    }else{
+
+        plan("sequential")
+
+    }
+
+    ## Set Tokens Locale
+    quanteda_options(tokens_locale = tokens_locale)
+    
+
+    ## Perform Calculations
+    lingstats <- f.future_lingsummarize(x)
+
+
+    ## Optional: Set German variable names
+    if(germanvars == TRUE){
+
+        setnames(lingstats,
+                 old = c("nchars",
+                         "ntokens",
+                         "ntypes",
+                         "nsentences"),
+                 new = c("zeichen",
+                         "tokens",
+                         "typen",
+                         "saetze"))
+
+    }
+
+
+    return(lingstats)
+
+    
+}
+
+
+
+#'# future_lingsummarize
+#' Iterated parallel computation of characters, tokens, types and sentences for each document of a given data table. Documents must contain text in a "text" variable and document names in a "doc_id" variable. The functionality is similar to textstats_summary() from the quanteda.textstats package, but this function is optimized for parallel processing of very large corpora.
+#'
+#' During computation documents are ordered by number of characters (descending) to ensure that long documents are computed first. For corpora with a skewed document length distribution this is significantly faster. The variables "nchars" is also added to the original object.
+
+
+# library(quanteda)
+# library(future)
+# library(future.apply)
+
+
+f.future_lingsummarize <- function(dt,
+                                 chunksperworker = 1,
+                                 chunksize = NULL){
+
+    begin.dopar <- Sys.time()
+
+    dt <- dt[,.(doc_id, text)]
+    
+    nchars <- dt[, lapply(.(text), nchar)]$V1
+    
+    print(paste0("Processing ",
+                 dt[,.N],
+                 " documents with a total length of ",
+                 sum(nchars),
+                 " characters."))
+
+    
+    ord <- order(-nchars)
+    dt <- dt[ord]
+
+    raw.list <- split(dt, seq(nrow(dt)))
+    
+    result.list <- future_lapply(raw.list,
+                                 f.lingsummarize,
+                                 future.seed = TRUE,
+                                 future.scheduling = chunksperworker,
+                                 future.chunk.size = chunksize)
+    
+    result.dt <- rbindlist(result.list)
+
+    
+
+    end.dopar <- Sys.time()
+    duration.dopar <- end.dopar - begin.dopar
+
+
+    summary.corpus <- cbind(nchars[ord],
+                            result.dt)
+
+    setnames(summary.corpus,
+             "V1",
+             "nchars")
+
+
+    if(dt["nchars" == 0, .N] > 0){
+        
+        dt.charnull <- dt["nchars" == 0]
+        dt.charnull$text <- NULL
+        dt.charnull$ntokens <- rep(0, dt.charnull[,.N])
+        dt.charnull$ntypes <- rep(0, dt.charnull[,.N])
+        dt.charnull$nsentences <- rep(0, dt.charnull[,.N])
+
+        summary.corpus <- rbind(summary.corpus,
+                                dt.charnull)
+    }
+
+    
+    summary.corpus <- summary.corpus[order(ord)]
+
+    
+    print(paste0("Runtime was ",
+                 round(duration.dopar,
+                       digits = 2),
+                 " ",
+                 attributes(duration.dopar)$units,
+                 ". Ended at ",
+                 end.dopar, "."))
+    
+    return(summary.corpus)
+
+}
+
+
+
+
+
+
+f.lingsummarize <- function(dt){
+
+    tryCatch({
+        
+        corpus <- corpus(dt)
+        
+        tokens <- tokens(corpus,
+                         what = "word",
+                         remove_punct = FALSE,
+                         remove_symbols = FALSE,
+                         remove_numbers = FALSE,
+                         remove_url = FALSE,
+                         remove_separators = TRUE,
+                         split_hyphens = FALSE,
+                         include_docvars = FALSE,
+                         padding = FALSE
+                         )
+        
+        ntokens <- unname(ntoken(tokens))
+        ntypes  <- unname(ntype(tokens))
+        nsentences <- unname(nsentence(corpus))
+
+        out <- data.table(ntokens,
+                          ntypes,
+                          nsentences)
+        
+        return(out)
+        
+    },
+    error = function(cond) {
+        return(data.table(ntokens = NA,
+                          ntypes = NA,
+                          nsentences = NA))}
+    )
+
+    
+}
+
